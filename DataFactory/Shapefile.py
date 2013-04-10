10600a0cac9219b869e62ca8bc365e3b4403667b
Chris Allen <cccchris.allen@gmail.com>
2013-04-10 15:42:29 -0700

    Creating Class to manipulate shapefiles.

---
+++ DataFactory/Shapefile.py
@@ -0,0 +1,33 @@
+
+
+class Shapefile(object):
+	
+	def __init__(self, fileName):
+		import fiona
+		
+		self._source = fiona.open(fileName, "r")
+		self._features = list(self._source)
+		
+		
+	def getBounds(self):
+		return self._source.bounds
+	
+	
+	def getFeatures(self):
+		return self._features
+		
+		
+	def saveAsSHP(self, outName):
+		output = fiona.open(outName, 'w', **self._source.meta)
+		outpu.writerecords(list(self._features))
+		
+		
+	def saveAsGeoJSON(self, outName):
+		import json
+		
+		featureCollection = dict(type="FeatureCollection", features=self._features, crs={})
+		
+		out = open(outName, "w")
+		out.write(json.dumps(featureCollection))
+
+			
